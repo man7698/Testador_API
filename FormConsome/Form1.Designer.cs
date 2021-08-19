f6b9ec77bde1d6a2f852302681ee38d29edcc821
man7698 <matheusalmeida76982gmail.com>
2021-08-19 17:21:07 -0300

    ok

---
+++ FormConsome/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace FormConsome
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridView1
+            // 
+            this.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView1.Location = new System.Drawing.Point(12, 12);
+            this.GridView1.Name = "GridView1";
+            this.GridView1.Size = new System.Drawing.Size(776, 226);
+            this.GridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Buscar Todos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(196, 316);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Buscar por id";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView GridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FormConsome/Form1.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace FormConsome
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+
+            buscartodos();
+
+
+
+
+        }
+
+
+
+        private async void buscartodos()
+        {
+            // using httpclient para fechar ao terminar de executar
+            using (var client = new HttpClient())
+            {
+
+                client.BaseAddress = new Uri("https://localhost:44319/api/usuarios");
+                var resposta = await client.GetAsync("");
+                var dados = await resposta.Content.ReadAsStringAsync();
+
+                var JsonConvert = new JavaScriptSerializer();
+
+                List<usuarios> u = JsonConvert.Deserialize<List<usuarios>>(dados);
+
+
+
+                GridView1.DataSource = u;
+
+
+
+
+
+            }
+
+
+
+        }
+
+
+        //private async void buscarporid(string cod)
+        //{
+        //   // using httpclient para fechar ao terminar de executar
+        //    using (var client = new HttpClient())
+        //    {
+
+        //        client.BaseAddress = new Uri("https://localhost:44319/api/usuarios/" + cod);
+        //        var resposta = await client.GetAsync("https://localhost:44319/api/usuarios/" + cod);
+
+        //        if (resposta.IsSuccessStatusCode)
+        //        {
+        //            string dados = await resposta.Content.ReadAsStringAsync();
+
+
+
+        //            var JsonConvert = new JavaScriptSerializer();
+
+        //            List<usuarios> u = JsonConvert.Deserialize<List<usuarios>>(dados);
+
+
+        //            GridView1.DataSource = u;
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("ERRO AO BUSCAR !!!!!!!!!!");
+        //        }
+
+
+
+
+
+
+        //    }
+
+
+
+
+
+        //}
+
+
+        private async Task buscarporid(string cod)
+        {
+            using(var client = new HttpClient())
+            {
+
+                BindingSource bsdados = new BindingSource();
+                client.BaseAddress = new Uri("https://localhost:44319/api/usuarios/"+cod);
+                var resposta = await client.GetAsync("");
+                
+
+                if(resposta.IsSuccessStatusCode)
+                {
+                    var dados = await resposta.Content.ReadAsStringAsync();
+
+                    bsdados.DataSource = JsonConvert.DeserializeObject<usuarios>(dados);
+
+                    this.GridView1.DataSource = bsdados;
+
+                }
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await buscarporid(textBox1.Text);
+        }
+    }
+
+
+
+
+    public class usuarios
+    {
+        public int cod_cliente { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+        public string cpf { get; set; }
+        public string telresidencial { get; set; }
+        public string telcelular { get; set; }
+        public string rg { get; set; }
+        public string email { get; set; }
+        public object cidade { get; set; }
+        public int cod_cidade { get; set; }
+    }
+
+    
+
+
+}
